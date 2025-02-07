c50e6654b9f2d9a346704b4b8e011afa9bb9ae5a
Angela Huang <angelaykhuang@gmail.com>
2025-02-07 18:03:02 -0500

    python

--- mac/mac.py
+++ mac/mac.py
@@ -1,37 +1,41 @@
-from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
+#venv\Scripts\Activate.ps1    
+from flask import Flask
 from flask_socketio import SocketIO, emit
+from aiortc import RTCPeerConnection, RTCSessionDescription
 import cv2
 import asyncio
+import eventlet
 
-socketio = SocketIO()
+app = Flask(__name__)
+socketio = SocketIO(app, async_mode="eventlet", cors_allowed_origins="*")
 
 pc = RTCPeerConnection()
 
-# Track for displaying video
-class VideoDisplayTrack(VideoStreamTrack):
-    def __init__(self, frame):
-        super().__init__()
-        self.frame = frame
-
-    async def recv(self):
-        return self.frame
-
-@socketio.on('offer')
-async def on_offer(data):
-    offer = RTCSessionDescription(data['sdp'], data['type'])
-    await pc.setRemoteDescription(offer)
-
-    # Create and send an answer
-    answer = await pc.createAnswer()
-    await pc.setLocalDescription(answer)
-    emit('answer', {'sdp': pc.localDescription.sdp, 'type': pc.localDescription.type})
-
-
-# Display the video frame
 def display_video(frame):
-    cv2.imshow('Video', frame.to_ndarray(format='bgr24'))
+    cv2.imshow('Video', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
+        
+async def handle_offer(offer):
+    await pc.setRemoteDescription(offer)
+    
+    @pc.on('track')
+    async def on_track(track):
+        if track.kind == 'video':
+            print("Receiving video track")
+            while True:
+                frame = await track.recv()
+                display_video(frame.to_ndarray(format='bgr24'))
+
+
+
+@socketio.on('offer')
+def on_offer(data):
+    print("Received offer:", data)
+
+    offer = RTCSessionDescription(data['sdp'], data['type'])
+    asyncio.run(handle_offer(offer))
+
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8000)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
